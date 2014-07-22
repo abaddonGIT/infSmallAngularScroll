06f363398087258302d746c911514e77d4f108c9
abaddonGIT <abaddongit@gmail.com>
2014-07-23 01:48:13 +0400

    full rebuild

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
 var app = angular.module('app', ['infinityScroll']);
-app.controller("baseController", ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
-
-    var offset = offset2 = 0;
+app.controller("baseController", ['$scope', '$http', '$rootScope', '$infinityScroll', function ($scope, $http, $rootScope, $infinityScroll) {
+    var offset = 0;
     $scope.scrollItems = [];
-    $scope.scrollItems2 = [];
-    //Запрос новых данных
-    $scope.scroll = function () {
+    var scroll = $infinityScroll.start({
+        external: false
+    });
+    scroll.result = function (go) {
         $http({ 'url': 'http://totpp.demosite.pro/slu/test.html?limit=60&offset=' + offset, 'method': 'get', 'headers': { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'} }).success(function (data) {
             var ln = data.length;
             if (!ln) {
-                $scope.accept = false;
+                go(0);
             } else {
                 for (var i = 0; i < ln; i++) {
                     $scope.scrollItems.push(data[i]);
                 }
                 offset += 60;
-                $scope.accept = true;
+                go(1);
             }
         });
-    }
-
-    $scope.after = function () {
-      console.log('after');
     };
+    var scroll2 = $infinityScroll.start({
+        'name': 'two'
+    });
+    console.log(scroll2);
 
-    $scope.before = function () {
-        console.log("before");
-    }
-
-    $scope.scroll2 = function () {
-        $http({ 'url': 'http://totpp.demosite.pro/slu/test.html?limit=60&offset=' + offset2, 'method': 'get', 'headers': { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'} }).success(function (data) {
-            var ln = data.length;
-            if (!ln) {
-                $scope.accept2 = false;
-            } else {
-                for (var i = 0; i < ln; i++) {
-                    $scope.scrollItems2.push(data[i]);
-                }
-                offset2 += 60;
-                $scope.accept2 = true;
-            }
-        });
-    }
+    //Запрос новых данных
+//    $scope.scroll = function () {
+//    $http({ 'url': 'http://totpp.demosite.pro/slu/test.html?limit=60&offset=' + offset, 'method': 'get', 'headers': { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'} }).success(function (data) {
+//        var ln = data.length;
+//        if (!ln) {
+//            $scope.accept = false;
+//        } else {
+//            for (var i = 0; i < ln; i++) {
+//                $scope.scrollItems.push(data[i]);
+//            }
+//            offset += 60;
+//            $scope.accept = true;
+//        }
+//    });
+//    }
+//
+//    $scope.scroll2 = function () {
+//        $http({ 'url': 'http://totpp.demosite.pro/slu/test.html?limit=60&offset=' + offset2, 'method': 'get', 'headers': { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'} }).success(function (data) {
+//            var ln = data.length;
+//            if (!ln) {
+//                $scope.accept2 = false;
+//            } else {
+//                for (var i = 0; i < ln; i++) {
+//                    $scope.scrollItems2.push(data[i]);
+//                }
+//                offset2 += 60;
+//                $scope.accept2 = true;
+//            }
+//        });
+//    }
 } ]);
--- js/scroll/scroll.js
+++ js/scroll/scroll.js
@@ -8,58 +8,95 @@
 (function (an, w, d) {
     "use strict";
     var infinityScroll = an.module("infinityScroll", [])
-        .directive("ngScrollPlace", ['$rootScope', '$timeout', function ($rootScope, $timeout) {
+        .value("$scrollInstance", {})
+        .directive("ngScrollPlace", ['$rootScope', '$timeout', '$scrollInstance', '$interval', function ($rootScope, $timeout, $scrollInstance, $interval) {
             return {
                 scope: {
-                    ngScrollPlace: "&",
-                    scrollSource: "@",
-                    scrollDisabled: "=?",
-                    scrollAfterDeadline: "&",
-                    scrollBeforeDeadline: "&",
-                    scrollHeightWatch: "=?"
+                    scrollDisabled: "=?"
                 },
                 link: function (scope, elem, attr) {
-                    var scrollEl, dH, allH, scrollTop, doc, big = false, small = true, timer;
-
-                    var scrollHandler = function () {
-                        scrollTop = w.pageYOffset || doc.scrollTop;
-                        allH = Math.max(doc.scrollHeight, dH);
-                        var contenerPath = dH + scrollTop; //прошедший путь
-                        if (scrollTop + 200 >= allH - dH && scope.scrollDisabled) {
-                            scope.scrollDisabled = false;
-                            scope.ngScrollPlace();
+                    var name = attr.ngScrollPlace || 'first';
+                    var timer = $interval(function () {
+                        var instance = $scrollInstance[name];
+                        if (instance) {
+                            instance.defer.resolve({'scope': scope, 'elem': elem});
+                            $interval.cancel(timer);
                         }
-                        if (scrollTop > scope.scrollHeightWatch && !big) {
-                            scope.scrollAfterDeadline();
-                            big = true;
-                            small = false;
-                        }
-                        if (scrollTop < scope.scrollHeightWatch && !small) {
-                            scope.scrollBeforeDeadline();
-                            big = false;
-                            small = true;
-                        }
-                    };
-                    var startScroll = function () {
-                        scrollEl = !scope.scrollSource ? an.element(w) : elem;
-                        doc = !scope.scrollSource ? d.documentElement : elem[0];
-                        dH = doc.clientHeight;
-
-                        scrollEl.off("scroll").on("scroll", function () {
-                            $timeout.cancel(timer);//ставим задержку для уменьшения расчетов
-                            timer = $timeout(scrollHandler, 100);
-                        });
-                        scope.ngScrollPlace();
-                    };
-                    $timeout(function () {
-                        scope.scrollDisabled = true;
-                        startScroll();
-                    }, 0);
+                    }, 100);
                 }
             };
-        } ]).filter("html", ['$sce', function ($sce) {
-            return function (input) {
-                return $sce.trustAsHtml(input);
+        } ]).factory('$infinityScroll', ['$rootScope', '$scrollInstance', '$q', '$timeout', function ($rootScope, $scrollInstance, $q, $timeout) {
+            var InfScroll = function (options) {
+                var contener, config, elemSource, dH, scrollTimer, scrollTop, wH;
+                if (!(this instanceof InfScroll)) {
+                    return new InfScroll(options);
+                }
+                an.extend(this, {
+                    timestamp: Date.now(),
+                    config: {},
+                    defer: $q.defer(),
+                    accept: true,
+                    result: function () {
+                    }
+                });
+                config = an.extend(this.config, {
+                    name: 'first',
+                    heightWatch: 500,
+                    external: true
+                }, options);
+
+                var result = function () {
+                    this.accept = false;
+                    var resultDefer = $q.defer();
+                    this.result(function (flag) {resultDefer.resolve(flag);});
+                    resultDefer.promise.then(function (flag) {
+                        flag ? this.accept = true : this.accept = false;
+                    }.bind(this));
+                };
+                var handler = function () {
+                    scrollTop = contener[0].pageYOffset || elemSource.scrollTop;
+                    wH = Math.max(elemSource.scrollHeight, dH);
+                    if (scrollTop + 200 >= wH - dH && this.accept) {
+                        result.call(this);
+                    }
+                };
+                var startScroll = function (directive) {
+                    if (config.external) {//Выбираем контенер со скролом
+                        contener = an.element(w);
+                        elemSource = d.documentElement;
+                    } else {
+                        contener = directive.elem;
+                        elemSource = directive.elem[0];
+                    }
+                    dH = elemSource.clientHeight;
+                    contener.off("scroll").on("scroll", function () {
+                        $timeout.cancel(scrollTimer);
+                        scrollTimer = $timeout(function () {//Задержка скрола
+                            handler.call(this);
+                        }.bind(this), 100);
+                    }.bind(this));
+                    result.call(this);
+                };
+                $scrollInstance[this.config.name] = this;
+
+                this.defer.promise.then(function (directive) {
+                    startScroll.call(this, directive);
+                }.bind(this));
             };
-        } ]);
+            InfScroll.prototype = {
+                bind: function (event, handler) {
+                    var name = event + ':' + this.timestamp;
+                    $rootScope.$on(name, handler.bind(this));
+                },
+                trigger: function (event) {
+                    arguments[0] = event + ':' + this.timestamp;
+                    $rootScope.$broadcast.apply($rootScope, arguments);
+                }
+            };
+            return {
+                start: function (options) {
+                    return InfScroll(options);
+                }
+            }
+        }]);
 })(angular, window, document);
